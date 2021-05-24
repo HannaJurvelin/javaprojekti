8535ec30db31e197ad36e2edfee52ab42bffec05
Hanna Jurvelin <>
2021-05-24 16:53:49 +0300

    Added intro text to a method

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
-// aja ohjelma fn + f5
+
+// aja ohjelma fn + f5, lopetus control + C
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        // Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        intro();
+
+        // int valinta = -1;
+
+        // while (valinta != 0) {
+        // valinta = myObj.nextInt();
+        // }
+    }
+
+    private static void intro() {
+        System.out.println("Your life was perfect until one day your dear mother passed away.\n"
+                + "A year later, your father married another woman.\n"
+                + "Your new step-mother turned out to be evil and greedy and her two daughters take after her.\n"
+                + "Soon after that your father also passes away because of an illness.\n"
+                + "You’re forced to be a servant to your stepmother and stepsisters, cooking and cleaning for them while they laze around the house.\n"
+                + "Until one day…");
     }
 }
